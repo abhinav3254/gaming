14797d42877c86b61733e283f8e1f72162f72dfb
abhinav3254 <abhinav3254@gmail.com>
2023-11-28 22:00:18 +0530

    bin to dec

--- utils/Binary.java
+++ utils/Binary.java
@@ -5,11 +5,13 @@ class Main {
 	public static void main(String[] args) {
 
 		int num = 45;
-		convertBinary(45);
+		List<Integer> list = convertBinary(45);
+
+		convertBinaryToDecimal(list);
 	}
 
 
-	public static void convertBinary(int num) {
+	public static List<Integer> convertBinary(int num) {
 
 		List<Integer> list = new ArrayList<>();
 
@@ -23,6 +25,19 @@ class Main {
 			System.out.print(list.get(i));
 		}
 
+		System.out.println();
+
+		return list;
+
+	}
+
+	public static void convertBinaryToDecimal(List<Integer> list) {
+		double ans = 0;
+		for(int i = 0;i<list.size();i++) {
+			ans = ans+list.get(i)*Math.pow(2,i);
+		}
+
+		System.out.print(ans);
 	}
 	
 }
