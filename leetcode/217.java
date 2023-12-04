e40cb2ea2d54a96e479ce8dc99566ef00a7087ab
Abhinav Jha <97444941+abhinav3254@users.noreply.github.com>
2023-12-04 13:08:23 +0530

    Update 217.java

--- leetcode/217.java
+++ leetcode/217.java
@@ -3,6 +3,8 @@
 The sorting approach sorts the array in ascending order and then checks for adjacent elements that are the same. 
 If any duplicates are found, it returns true. Sorting helps in bringing duplicates together, simplifying the check. However, sorting has a time complexity of O(n log n).
 
+Used HashSet because it reduces the time complexity from O(nlogn) to O(n).
+
 */
 
 class Solution {
@@ -18,10 +20,21 @@ class Solution {
 
         // return false;
 
-        Arrays.sort(nums);
 
-        for (int i = 1;i<nums.length;i++) {
-            if (nums[i] == nums[i-1]) return true;
+        // not full optomized
+        // Arrays.sort(nums);
+
+        // for (int i = 1;i<nums.length;i++) {
+        //     if (nums[i] == nums[i-1]) return true;
+        // }
+        // return false;
+
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+            seen.add(num);
         }
         return false;
     }
