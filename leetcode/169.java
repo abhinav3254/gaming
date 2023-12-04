707c2d7d7a2ae2ef34c6e6781c6e1594d26ae042
Abhinav Jha <97444941+abhinav3254@users.noreply.github.com>
2023-12-04 13:35:31 +0530

    Update 169.java

--- leetcode/169.java
+++ leetcode/169.java
@@ -1,13 +1,37 @@
 /*
 What I thought was that whatever will be the majority it must be more than mid of the array,
-so return the mid index value of the array
+so return the mid index value of the array.
+
+
+Using boyer-moore-majority-voting-algorithm
+time complexity of O(n) and a space complexity of O(1).
+
 */
+
 
 class Solution {
     public int majorityElement(int[] nums) {
 
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        // O(n log n) for sorting
+
+        // Arrays.sort(nums);
+        // return nums[nums.length/2];
         
+        // Using boyer-moore-majority-voting-algorithm
+        //  time complexity of O(n) and a space complexity of O(1)
+
+        int candidate = nums[0];
+        int count = 1;
+
+        for (int i = 1;i<nums.length;i++) {
+            if (count == 0) {
+                candidate = nums[i];
+                count = 1;
+            } else if (nums[i] == candidate) count++;
+            else count--;
+        }
+
+        return candidate;
+
     }
 }
