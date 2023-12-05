8f8479ed01cd0bf3aca0aef6df3a2032c6cb7e47
abhinav3254 <abhinav3254@gmail.com>
2023-12-05 21:34:51 +0530

    fixed

--- Codeforces Round 913 (Div. 3)/A.java
+++ Codeforces Round 913 (Div. 3)/A.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
- 
-public class One {
+
+class One {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
