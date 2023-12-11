fc7c6ae65b1e33f0a0d8b718483028c0a41b4496
abhinav3254 <abhinav3254@gmail.com>
2023-12-11 20:30:02 +0530

    263 optimizated

--- leetcode/263.java
+++ leetcode/263.java
@@ -11,7 +11,16 @@ class Main {
 
 	}
 
-	
+	public static boolean isUglyOptimized(int n) {
+		if (n <= 0) return false;
+
+		while (n%2 == 0) n /= 2;
+		while (n%3 == 0) n /= 3;
+		while (n%5 == 0) n /= 5;
+
+		return n == 1;
+
+	}
 
 	public static boolean isUgly(int n) {
 		List<Integer> list = getAllPrime(n);
