3dea52fd395c61aeefeeb4dd645bed0351e59ee7
abhinav3254 <abhinav3254@gmail.com>
2023-12-11 20:43:21 +0530

    un optimized solution

---
+++ leetcode/264.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(nthUglyNumber(n));
+	}
+
+	public static int nthUglyNumber(int n) {
+		int count = 0; 
+		int i = 1;
+        while (count <= n) {
+        	if (isUglyOptimized(i)) {
+        		count++;
+        	}
+        	if (count == n) {
+        		return i;
+        	}
+        	i++;
+        }
+        return 0;
+    }
+	
+
+	public static boolean isUglyOptimized(int n) {
+		if (n <= 0) return false;
+
+		while (n%2 == 0) n /= 2;
+		while (n%3 == 0) n /= 3;
+		while (n%5 == 0) n /= 5;
+
+		return n == 1;
+
+	}
+}
