7595a88f3e1b7fa3d261036ca640433ec0f992d5
abhinav3254 <abhinav3254@gmail.com>
2023-12-12 06:41:26 +0530

    isPalindrome number check

---
+++ rough/2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		System.out.println(isPalindrome(s,0,s.length()-1));
+	}
+
+	private static boolean isPalindrome(String s,int l,int r) {
+		if (l>=r) return true;
+		if (s.charAt(l) == s.charAt(r))
+			return isPalindrome(s,++l,--r);
+		else return false;
+	}
+}
