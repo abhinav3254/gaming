111d0b46eca48a0c7fd3da02f2a0a2872737231a
abhinav3254 <abhinav3254@gmail.com>
2023-12-12 06:24:47 +0530

    practise recursion calls

--- rough/1.java
+++ rough/1.java
@@ -6,10 +6,23 @@ class Main {
 		int n = sc.nextInt();
 
 		System.out.println("sum of "+n+" natural numbers are :- "+findSumOfNNaturalNumber(n));
+		System.out.println("factorial of "+n+" is :- "+factorialOfNumber(n));
 	}
 
+
+	/**
+	 * formula for sum of n natural number is :- n(n+1)/2
+	 * 
+	 * @author AbKumar
+	 */
 	private static int findSumOfNNaturalNumber(int n) {
 		if (n == 0) return 0;
 		return n + findSumOfNNaturalNumber(n-1);
 	}
+
+	private static int factorialOfNumber (int n) {
+		if (n<=1) return 1;
+		return n*factorialOfNumber(n-1);
+	}
+
 }
