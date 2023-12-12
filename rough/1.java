008e5c71401579cd9e1f0ebf6eef8ac47ccb0b37
abhinav3254 <abhinav3254@gmail.com>
2023-12-12 06:30:52 +0530

    Fibonacci and approach of dealing questions

--- rough/1.java
+++ rough/1.java
@@ -7,6 +7,7 @@ class Main {
 
 		System.out.println("sum of "+n+" natural numbers are :- "+findSumOfNNaturalNumber(n));
 		System.out.println("factorial of "+n+" is :- "+factorialOfNumber(n));
+		System.out.println("fib of "+n+"th index is :- "+fib(n));
 	}
 
 
@@ -14,6 +15,11 @@ class Main {
 	 * formula for sum of n natural number is :- n(n+1)/2
 	 * 
 	 * @author AbKumar
+	 * 
+	 * What is the common approach I've used here is that
+	 * always think of just small number like find the sum of first
+	 * 2 natural number , same for factorial of 2 only 
+	 * and in Fibonacci same thought to find the fibonacci of just 2
 	 */
 	private static int findSumOfNNaturalNumber(int n) {
 		if (n == 0) return 0;
@@ -23,6 +29,12 @@ class Main {
 	private static int factorialOfNumber (int n) {
 		if (n<=1) return 1;
 		return n*factorialOfNumber(n-1);
+	}
+
+	private static int fib(int n) {
+		if (n == 0) return 0;
+		if (n == 1) return 1;
+		return fib(n-1) + fib(n-2); 
 	}
 
 }
