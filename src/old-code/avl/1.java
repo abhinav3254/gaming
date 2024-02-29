e327f4e52676339062b88824284b4c4cba0da9fd
abhinav3254 <abhinav3254@gmail.com>
2024-02-29 23:35:35 +0530

    issue resolved

--- src/old-code/avl/1.java
+++ src/old-code/avl/1.java
@@ -1,15 +1,13 @@
 
-
-
 class Main {
 
 	/*
-	*
-	*Creating a node class to store elements
-	*Node class will have a data
-	*left will be pointing to left sub tree
-	* and right will be pointing to right sub tree
-	**/
+	 *
+	 * Creating a node class to store elements
+	 * Node class will have a data
+	 * left will be pointing to left sub tree
+	 * and right will be pointing to right sub tree
+	 **/
 	static class Node {
 		int data;
 		Node left;
@@ -22,18 +20,18 @@ class Main {
 		}
 	}
 
-	public static Node insert(Node node ,int data) {
-		if (node == null) { return new Node(data); }
+	// public static Node insert(Node node ,int data) {
+	// if (node == null) { return new Node(data); }
 
-		if (data<node.data) {
-			// left insert
-			node.left = insert(node.left,data);
-		} else if (data>node.data) {
-			// right insert
-			node.right = insert(node.right,data);
-		} else { return node; }  // duplicate entry, not allowed
+	// if (data<node.data) {
+	// // left insert
+	// node.left = insert(node.left,data);
+	// } else if (data>node.data) {
+	// // right insert
+	// node.right = insert(node.right,data);
+	// } else { return node; } // duplicate entry, not allowed
 
-		// now data is inserted, it's time to calculate the k value
-		// k value means height(leftsubtree)-height(rightsubtree)
-	}
+	// // now data is inserted, it's time to calculate the k value
+	// // k value means height(leftsubtree)-height(rightsubtree)
+	// }
 }
